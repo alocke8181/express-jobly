60b2027e2781b06284a0f0854b37f183f44f8716
alocke8181 <alocke_8181@yahoo.com>
2024-01-20 13:22:59 -0500

    changed for render deploy

--- db.js
+++ db.js
@@ -7,8 +7,9 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    host: "/var/run/postgresql",
-    database: getDatabaseUri(),
+    //host: "/var/run/postgresql",
+    //database: getDatabaseUri(),
+    connectionString : getDatabaseUri();
     ssl: {
       rejectUnauthorized: false
     }
